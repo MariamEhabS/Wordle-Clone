74d583a0734a85553e64c87684502d520cd63777
Mariam Ehab Codes <64708267+MariamEhabS@users.noreply.github.com>
2022-06-29 02:28:46 +0200

    added game logic

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -4,22 +4,56 @@ const useWordle = (solution) => {
 
     const [turn, setTurn] = useState(0)
     const [currentGuess, setCurrentGuess] = useState('')
-    const [guesses, setGuesses] = useState([]) //each guess is an array
-    const [history, setHistory] = useState([]) // each guess is a string 
+    const [guesses, setGuesses] = useState([...Array(6)]) //each guess is an array
+    const [history, setHistory] = useState(['hello', 'ninja']) // each guess is a string 
     const [isCorrect, setIsCorrect] = useState(false)
 
 
     // we need to create a function that will format a guess into an array of letter objects
     //e.g. [{key: 'a', color: 'yellow'}]
     const formatGuess = () => {
+        console.log(`formatting the guess ${currentGuess}`) 
+        let solutionArray = [...solution]
+        let formattedGuess = [...currentGuess].map((l) => {
+        return {key: l, color: 'grey'}
+        })
 
+        //find any letter that needs to be formatted green
+        formattedGuess.forEach((l, i) => {
+            if(solutionArray[i] === l.key){
+                formattedGuess[i].color = 'green'
+                solutionArray[i] = null 
+            }
+        })
+
+        formattedGuess.forEach((l,i) => {
+            if(solutionArray.includes(l.key) && l.color !== 'green'){
+                formattedGuess[i].color = 'yellow'
+                solutionArray[solutionArray.indexOf(l.key)] = null
+            }
+        })
+        return formattedGuess
     }
 
     // need to create another function that takes user guesses and adds it to a history of guesses stored in guesses state
     //check if guesses are correct and store that in state 
     // add on to turn number every guess 
-    const addNewGuess = () => {
-
+    const addNewGuess = (formattedGuess) => {
+        if (currentGuess === solution) {
+            setIsCorrect(true)
+        }
+        setGuesses((prevGuesses) => {
+            let newGuesses = [...prevGuesses]
+            newGuesses[turn] = formattedGuess
+            return newGuesses
+        })
+        setHistory((prevHistory) => {
+            return [...prevHistory, currentGuess]
+        })
+        setTurn((prevTurn) => {
+            return prevTurn + 1
+        })
+        setCurrentGuess('')
     }
 
     //another function to handle keyup event and to also track currnt guess 
@@ -29,9 +63,21 @@ const useWordle = (solution) => {
 
         if(key === 'Enter'){
             // only add guess if turn is less than 5 
-            
+            if(turn > 5){
+                console.log(' You used all your guesses')
+                return 
+            }
             // dont allow duplicate words
+            if(history.includes(currentGuess)){
+                console.log(`You've already tried this guess`)
+                return 
+            }
             //the word must be 5 chars long 
+            if(currentGuess.length !== 5 ){
+                console.log('Word must be 5 chars long');
+            }
+           const formatted =  formatGuess()
+            addNewGuess(formatted)
         }
         if(key === 'Backspace'){
             setCurrentGuess((prev) => {
