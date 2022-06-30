028f06a05dbbdbd9d5ba58db698e178c07b37237
Mariam Ehab Codes <64708267+MariamEhabS@users.noreply.github.com>
2022-07-01 01:49:09 +0200

    added keypad

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -5,14 +5,13 @@ const useWordle = (solution) => {
     const [turn, setTurn] = useState(0)
     const [currentGuess, setCurrentGuess] = useState('')
     const [guesses, setGuesses] = useState([...Array(6)]) //each guess is an array
-    const [history, setHistory] = useState(['hello', 'ninja']) // each guess is a string 
+    const [history, setHistory] = useState([]) // each guess is a string 
     const [isCorrect, setIsCorrect] = useState(false)
-
+    const [usedKeys, setUsedKeys] = useState({})
 
     // we need to create a function that will format a guess into an array of letter objects
     //e.g. [{key: 'a', color: 'yellow'}]
     const formatGuess = () => {
-        console.log(`formatting the guess ${currentGuess}`) 
         let solutionArray = [...solution]
         let formattedGuess = [...currentGuess].map((l) => {
         return {key: l, color: 'grey'}
@@ -20,7 +19,7 @@ const useWordle = (solution) => {
 
         //find any letter that needs to be formatted green
         formattedGuess.forEach((l, i) => {
-            if(solutionArray[i] === l.key){
+            if(solution[i] === l.key){
                 formattedGuess[i].color = 'green'
                 solutionArray[i] = null 
             }
@@ -42,7 +41,7 @@ const useWordle = (solution) => {
         if (currentGuess === solution) {
             setIsCorrect(true)
         }
-        setGuesses((prevGuesses) => {
+        setGuesses(prevGuesses => {
             let newGuesses = [...prevGuesses]
             newGuesses[turn] = formattedGuess
             return newGuesses
@@ -53,9 +52,29 @@ const useWordle = (solution) => {
         setTurn((prevTurn) => {
             return prevTurn + 1
         })
+        setUsedKeys((prevUsedKeys) => {
+            let newKeys = {...prevUsedKeys}
+
+            formattedGuess.forEach((l) => {
+                const currentColor = newKeys[l.key]
+
+                if (l.color === 'green') {
+                    newKeys[l.key] = 'green'
+                    return 
+                }
+                if (l.color === 'yellow' && currentColor !== 'green') {
+                    newKeys[l.key] = 'yellow'
+                    return
+                }
+                if (l.color === 'grey' && currentColor !== 'green' && currentColor !== 'yellow'){
+                    newKeys[l.key] = 'grey'
+                    return
+                }
+            })
+            return newKeys
+        })
         setCurrentGuess('')
     }
-
     //another function to handle keyup event and to also track currnt guess 
     //and detect if enter button is pressed to submit guess 
     const handleKeyup = ({ key }) => {
@@ -64,7 +83,7 @@ const useWordle = (solution) => {
         if(key === 'Enter'){
             // only add guess if turn is less than 5 
             if(turn > 5){
-                console.log(' You used all your guesses')
+                console.log('You used all your guesses')
                 return 
             }
             // dont allow duplicate words
@@ -75,6 +94,7 @@ const useWordle = (solution) => {
             //the word must be 5 chars long 
             if(currentGuess.length !== 5 ){
                 console.log('Word must be 5 chars long');
+                return
             }
            const formatted =  formatGuess()
             addNewGuess(formatted)
@@ -83,7 +103,7 @@ const useWordle = (solution) => {
             setCurrentGuess((prev) => {
                 return prev.slice(0,-1)
             })
-            return 
+            return  
         }
         if (/^[A-za-z]$/.test(key)) {
             if(currentGuess.length < 5){
@@ -93,7 +113,7 @@ const useWordle = (solution) => {
             }
         }
     }
-    return {turn, currentGuess, guesses, isCorrect, handleKeyup}
+    return {turn, currentGuess, guesses, isCorrect, usedKeys, handleKeyup}
 }
 
 export default useWordle
